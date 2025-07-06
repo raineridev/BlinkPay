205376473bd2cb3da0f57eb026c430ac1a4aa4dd
Marcelo Raineri <mraineridev@gmail.com>
2025-07-06 16:50:10 -0300

    feat(ui): add InputField component with styling

---
+++ src/components/ui/InputField.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+import { ref } from 'vue';
+
+const props = defineProps<{
+//
+}>()
+
+</script>
+
+<template>
+    <input 
+    type="text" 
+    class="w-full rounded-md border-1 border-gray-300 px-3 py-2 text-base text-gray-900 placeholder-gray-400 shadow-sm focus:border-green-500 focus:outline-none transition">
+</template>
